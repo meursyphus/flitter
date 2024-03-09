0d5c9ed047dca2054788bfae45b1d2b42abf4fe0
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-03-09 16:28:41 +0900

    add ui frameworks

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), svelte(), tailwind()]
+});
