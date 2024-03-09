5f92fe5cf99fcd1ba873e4d5233c801a8aeb3108
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-03-09 23:04:38 +0900

    sidebar 추가

--- packages/docs/src/content/config.ts
+++ packages/docs/src/content/config.ts
@@ -3,10 +3,10 @@ import { defineCollection, reference, z } from "astro:content";
 const docs = defineCollection({
   type: "content", // v2.5.0 이후 버전에서 사용
   schema: z.object({
-    "nav-group": z.string(),
-    "nav-group-order": z.number(),
-    "nav-order": z.number(),
-    "nav-title": z.string().optional(),
+    nav_group: z.string(),
+    nav_group_order: z.number(),
+    nav_order: z.number(),
+    nav_title: z.string().optional(),
     title: z.string(),
     description: z.string().optional(),
     tags: z.array(z.string()).optional(),
