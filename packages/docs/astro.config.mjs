511af00543281569178c6657cccbde75baceec96
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-03-09 16:46:57 +0900

    add prettier

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), tailwind()]
+  integrations: [react(), svelte(), tailwind()],
 });
