d42892f6e9ed583cdebd60617926d8ca8b18ec61
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-03-10 10:17:14 +0900

    [MS-7]  docs init (#2)
    * init
    
    * 404
    
    * setting is done with sample .mdx
    
    * init mdx file
    
    * sidebar 추가
    
    * previous next 추가
    
    * init toc
    
    * add typography with tailwind
    
    * add code highlighter
    
    * toc link view 추가
    
    * sidebar design
    
    * scroll bar design
    
    * header 흐에
    
    * hh

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,10 +1,36 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), tailwind()],
+  integrations: [react(), svelte(), tailwind(), mdx()],
+  redirects: {
+    "/docs": "/docs/introduction",
+  },
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+      // Alternatively, provide multiple themes
+      // https://shiki.style/guide/dual-themes#light-dark-dual-themes
+      experimentalThemes: {
+        light: "github-dark",
+        dark: "github-light",
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
 });
