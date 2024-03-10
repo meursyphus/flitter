9642341052d335f091c758735668d910befb37ca
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-03-10 09:07:24 +0900

    add code highlighter

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -11,4 +11,26 @@ export default defineConfig({
   redirects: {
     "/docs": "/docs/introduction",
   },
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+      // Alternatively, provide multiple themes
+      // https://shiki.style/guide/dual-themes#light-dark-dual-themes
+      experimentalThemes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
 });
