483d6cb95f9be50d777bccae4b6a53988a0c9bd5
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-03-16 02:19:50 +0900

    commit

--- packages/story/src/stories/Column/Column.stories.ts
+++ packages/story/src/stories/Column/Column.stories.ts
@@ -7,7 +7,8 @@ import {
 	Flexible,
 	VerticalDirection,
 	Alignment,
-	TextStyle
+	TextStyle,
+	MainAxisAlignment
 } from '@meursyphus/flitter';
 import { dedent } from 'ts-dedent';
 const ImportWidgetCode = dedent`import { Column, Container, Flexible } from '@meursyphus/flitter';
@@ -198,7 +199,8 @@ export const MainAxisAlignment_center: Story = {
 		widget: Container({
 			color: 'lightblue',
 			child: Column({
-				mainAxisAlignment: 'center',
+
+				mainAxisAlignment: MainAxisAlignment.center,
 				children: [
 					Container({
 						width: 50,
