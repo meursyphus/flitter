5c9e368236324483b62a92690a0c80d8e952e5ae
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-03-26 02:14:27 +0900

    asdf

--- packages/flitter/src/scheduler/RenderFrameDispatcher.ts
+++ packages/flitter/src/scheduler/RenderFrameDispatcher.ts
@@ -8,16 +8,10 @@ class RenderFrameDispatcher {
     this.onFrame = () => callback();
   }
 
-  // Actually we don't need to invoke browser to render because browser automatically render its own state periodically
-  // so Here we just call onFrame callback.
-  private idle = true;
   dispatch() {
     if (typeof window === "undefined") return;
-    if (!this.idle) return;
-    this.idle = false;
     window.requestAnimationFrame(() => {
       this.onFrame?.();
-      this.idle = true;
     });
   }
 }
