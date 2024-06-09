96e145ba5337d8f744d880ab1c9ab9d5aee8e1e8
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-06-09 12:51:08 +0900

    good

--- packages/flitter-svelte/src/routes/+page.svelte
+++ packages/flitter-svelte/src/routes/+page.svelte
@@ -3,4 +3,12 @@
 	import App from './app';
 </script>
 
-<Widget width="100%" height="100%" widget={App()} />
+<Widget width="100%" height="100vh" widget={App()} />
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		height: 100vh;
+	}
+</style>
