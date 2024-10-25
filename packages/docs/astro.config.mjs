d1f0e8ca0a95eb8ebdc39ed000b6c1fdf081d71f
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-10-25 16:33:28 +0900

    save

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -5,10 +5,14 @@ import tailwind from "@astrojs/tailwind";
 //import ts from '@ast'
 
 import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), tailwind(), mdx()],
+  site: "https://flitter.dev",
+  integrations: [react(), svelte(), tailwind(), mdx(), sitemap({
+    lastmod: new Date('2024-10-25')
+  })],
   redirects: {
     "/docs": "/docs/introduction",
     "/tutorial": "/tutorial/introduction",
