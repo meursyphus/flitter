e55546c4784d442ce4b7951e2988b5ced7960be6
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-12-07 17:14:24 +0900

    good

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -10,7 +10,9 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: "https://flitter.dev",
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+
+  }),
   integrations: [
     react(),
     svelte(),
