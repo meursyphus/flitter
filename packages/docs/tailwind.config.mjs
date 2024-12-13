62cafc9b6a3af4607b0b934941af0d8777530b23
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-12-13 19:32:11 +0900

    feat : Theme Toggle 작성

--- packages/docs/tailwind.config.mjs
+++ packages/docs/tailwind.config.mjs
@@ -1,6 +1,7 @@
 import typography from "@tailwindcss/typography";
 
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
