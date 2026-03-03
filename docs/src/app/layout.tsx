54cbad2d18ff54bb5c4e72a287e40b07d4274b20
daeseung moon <tmdeoans@snu.ac.kr>
2026-03-03 14:32:20 +0900

    asdf

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
-import { DM_Sans, Instrument_Serif } from "next/font/google";
+import localFont from "next/font/local";
+import { Instrument_Serif } from "next/font/google";
 import Header from "@/components/Header";
 import GlobalSidebar from "@/components/GlobalSidebar";
 import "./globals.css";
 
-const dmSans = DM_Sans({ subsets: ["latin"], variable: "--font-body" });
+const pretendard = localFont({
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-body",
+});
 const instrumentSerif = Instrument_Serif({
   weight: "400",
   subsets: ["latin"],
@@ -23,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${dmSans.variable} ${instrumentSerif.variable}`}>
+    <html lang="en" className={`${pretendard.variable} ${instrumentSerif.variable}`}>
       <body className="bg-white text-neutral-900 antialiased" style={{ fontFamily: "var(--font-body)" }}>
         <Header />
         <div className="flex">
---
+++ packages/chart/TODO.md
@@ -0,0 +1,8 @@
+HEATMAP, TREEMAP
+
+그다음에 RADAR와 PIE는 AG 버전을 둘것? 
+- HEATMAP과 TREEMAP을 해놔야하나 
+
+SUNBURST, WATERFALL, TREEMAP, FUNNEL, CANDLESTICK -> 이것은 잘 안보일지도?
+
+그러고 나면 docs에 대해 폰트를? 
